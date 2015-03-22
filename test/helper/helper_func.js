4ffdf75dd2b9850eb8c5fb6094624feb4d7bdb27
iMumuMua <imumu@foxmail.com>
2015-03-22 11:31:54 +0800

    fix parallel bug

--- test/helper/helper_func.js
+++ test/helper/helper_func.js
@@ -4,57 +4,55 @@
 
 var Q = require('q');
 
-exports.singleAsyncFunc = function (callback) {
-  setTimeout(function () {
+exports.singleAsyncFunc = function(callback) {
+  setTimeout(function() {
     callback();
   }, 10);
 };
 
-exports.singleAsyncFailFunc = function (callback) {
-  setTimeout(function () {
+exports.singleAsyncFailFunc = function(callback) {
+  setTimeout(function() {
     callback(new Error());
   }, 10);
 };
 
-exports.asyncFunc = function (arg, callback) {
-  setTimeout(function () {
+exports.asyncFunc = function(arg, callback) {
+  setTimeout(function() {
     callback(null, arg);
   }, 10);
 };
 
-exports.asyncFailFunc = function (arg, callback) {
-  setTimeout(function () {
+exports.asyncFailFunc = function(arg, callback) {
+  setTimeout(function() {
     callback(new Error('async error'));
   }, 10);
 };
 
-exports.asyncTimerFunc = function (millisecond, callback) {
-  setTimeout(function () {
+exports.asyncTimerFunc = function(millisecond, callback) {
+  setTimeout(function() {
     callback(null);
   }, millisecond);
 };
 
-exports.asyncMultiArgsFunc = function (arg1, arg2, arg3, callback) {
-  setTimeout(function () {
+exports.asyncMultiArgsFunc = function(arg1, arg2, arg3, callback) {
+  setTimeout(function() {
     callback(null, arg1, arg2, arg3);
   }, 10);
 };
 
-exports.asyncMultiArgsFailFunc = function (arg1, arg2, arg3, callback) {
-  setTimeout(function () {
+exports.asyncMultiArgsFailFunc = function(arg1, arg2, arg3, callback) {
+  setTimeout(function() {
     callback(new Error('multi args async error'));
   }, 10);
 };
 
-exports.createPromise = function (data, isThrowError) {
-  var deferred = Q.defer();
-  setTimeout(function () {
+exports.createPromise = function(data, isThrowError) {
+  return Q.fcall(function() {
     if (isThrowError) {
-      deferred.reject(new Error('promise error'));
+      throw new Error('promise error');
     }
     else {
-      deferred.resolve(data);
+      return data;
     }
-  }, 10);
-  return deferred.promise;
+  });
 };
--- test/parallel.js
+++ test/parallel.js
@@ -1,204 +1,206 @@
 var tiny = require('../lib/tiny.js');
 var helper = require('./helper/helper_func.js');
 
-describe('parallel', function () {
+describe('parallel', function() {
 
-  it('single test', function () {
+  it('single test', function() {
     var ctrl = new tiny.Controller();
     var testData = {};
-    ctrl.parallel(function () {
-        testData.stepOne = 'one';
-      })
-      .parallel(function () {
-        testData.stepTwo = 'two';
-      })
-      .run();
-    testData.stepOne.should.equal('one');
-    testData.stepTwo.should.equal('two');
+    ctrl.parallel(function() {
+      testData.stepOne = 'one';
+    });
+    ctrl.parallel(function() {
+      testData.stepTwo = 'two';
+    });
+    ctrl.run(function() {
+      testData.stepOne.should.equal('one');
+      testData.stepTwo.should.equal('two');
+    });
+
   });
 
-  it('async timer test', function (done) {
+  it('async timer test', function(done) {
     var ctrl = new tiny.Controller();
     var testData = [];
-    ctrl.parallel(helper.asyncTimerFunc, 30, function () {
-        testData.push('first');
-      })
-      .parallel(helper.asyncTimerFunc, 15, function () {
-        testData.push('two');
-      })
-      .run(function () {
-        testData[0].should.equal('two');
-        testData[1].should.equal('first');
-        done();
-      });
+    ctrl.parallel(helper.asyncTimerFunc, 30, function() {
+      testData.push('first');
+    });
+    ctrl.parallel(helper.asyncTimerFunc, 15, function() {
+      testData.push('two');
+    });
+    ctrl.run(function() {
+      testData[0].should.equal('two');
+      testData[1].should.equal('first');
+      done();
+    });
   });
 
-  it('promise test', function (done) {
+  it('promise test', function(done) {
     var ctrl = new tiny.Controller();
     var testData = {};
-    ctrl.parallel(helper.createPromise('pr'), function (data) {
-        data.should.equal('pr');
-        testData.pr = data;
-      })
-      .parallel(helper.createPromise('pr2'), function (data) {
-        data.should.equal('pr2');
-        testData.pr2 = data;
-      })
-      .run(function () {
-        testData.pr.should.equal('pr');
-        testData.pr2.should.equal('pr2');
-        done();
-      });
+    ctrl.parallel(helper.createPromise('pr'), function(data) {
+      data.should.equal('pr');
+      testData.pr = data;
+    });
+    ctrl.parallel(helper.createPromise('pr2'), function(data) {
+      data.should.equal('pr2');
+      testData.pr2 = data;
+    });
+    ctrl.run(function() {
+      testData.pr.should.equal('pr');
+      testData.pr2.should.equal('pr2');
+      done();
+    });
   });
 
-  it('nested test', function (done) {
+  it('nested test', function(done) {
     var ctrl = new tiny.Controller();
     var nestedData = {};
-    ctrl.parallel(function () {
-        var ctrl = new tiny.Controller();
-        ctrl.parallel(function () {
-          nestedData.single = true;
-        });
-        return ctrl;
-      })
-      .parallel(helper.singleAsyncFunc, function () {
-        var ctrl = new tiny.Controller();
-        ctrl.parallel(function () {
-          nestedData.singleAsyncFunc = true;
-        });
-        return ctrl;
-      })
-      .parallel(helper.asyncFunc, 'async', function (data) {
-        var ctrl = new tiny.Controller();
-        ctrl.parallel(function () {
-          nestedData.asyncFunc = true;
-        });
-        return ctrl;
-      })
-      .parallel(helper.createPromise('pr'), function (data) {
-        var ctrl = new tiny.Controller();
-        ctrl.parallel(function () {
-          nestedData.promise = true;
-        });
-        return ctrl;
-      })
-      .run(function () {
-        for (var key in nestedData) {
-          nestedData[key].should.be.true;
-        }
-        done();
+    ctrl.parallel(function() {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(function() {
+        nestedData.single = true;
       });
+      return ctrl;
+    });
+    ctrl.parallel(helper.singleAsyncFunc, function() {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(function() {
+        nestedData.singleAsyncFunc = true;
+      });
+      return ctrl;
+    });
+    ctrl.parallel(helper.asyncFunc, 'async', function(data) {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(function() {
+        nestedData.asyncFunc = true;
+      });
+      return ctrl;
+    });
+    ctrl.parallel(helper.createPromise('pr'), function(data) {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(function() {
+        nestedData.promise = true;
+      });
+      return ctrl;
+    });
+    ctrl.run(function() {
+      for (var key in nestedData) {
+        nestedData[key].should.be.true;
+      }
+      done();
+    });
   });
 
-  describe('error test', function () {
-    it('single func', function (done) {
+  describe('error test', function() {
+    it('single func', function(done) {
       var ctrl = new tiny.Controller();
-      ctrl.parallel(function () {
-          throw new Error('aa');
-        })
-        .onError(function (err) {
-          err.message.should.equal('aa');
-          done();
-        })
-        .run(function () {
-          should(true).be.false;
-        });
-    });
-
-    it('single async', function (done) {
-      var ctrl = new tiny.Controller();
-      ctrl.parallel(helper.singleAsyncFunc, function () {
-          throw new Error('aa');
-        })
-        .onError(function (err) {
-          err.message.should.equal('aa');
-          done();
-        })
-        .run(function () {
-          should(true).be.false;
-        });
-    });
-
-    it('multi args async', function (done) {
-      var ctrl = new tiny.Controller();
-      ctrl.parallel(helper.asyncMultiArgsFunc, 1, 2, 3, function () {
-          throw new Error('aa');
-        })
-        .onError(function (err) {
-          err.message.should.equal('aa');
-          done();
-        })
-        .run(function () {
-          should(true).be.false;
-        });
-    });
-
-    it('promise fail', function (done) {
-      var ctrl = new tiny.Controller();
-      ctrl.parallel(helper.createPromise('pr', true), function () {
-          throw new Error('aa');
-        })
-        .onError(function (err) {
-          err.message.should.equal('promise error');
-          done();
-        })
-        .run(function () {
-          should(true).be.false;
-        });
-    });
-
-    it('promise throw', function (done) {
-      var ctrl = new tiny.Controller();
-      ctrl.parallel(helper.createPromise('pr'), function () {
-          throw new Error('aa');
-        })
-        .onError(function (err) {
-          err.message.should.equal('aa');
-          done();
-        })
-        .run(function () {
-          should(true).be.false;
-        });
-    });
-
-  });
-
-
-
-  describe('nested error test', function () {
-    it('should use root error handler if sub tiny error handler is default', function (done) {
-      var ti = new tiny.Controller();
-      ti.parallel(function () {
-        var subti = new tiny.Controller();
-        subti.parallel(function () {
-          throw new Error('subti');
-        });
-        return subti;
+      ctrl.parallel(function() {
+        throw new Error('aa');
       });
-      ti.onError(function (err) {
-        err.message.should.equal('subti');
+      ctrl.onError(function(err) {
+        err.message.should.equal('aa');
         done();
       });
-      ti.run();
-    });
-
-    it('should use sub error handler if sub tiny error handler is inited', function (done) {
-      var ti = new tiny.Controller();
-      ti.parallel(function () {
-        var subti = new tiny.Controller();
-        subti.parallel(function () {
-          throw new Error('subti');
-        });
-        subti.onError(function (err) {
-          err.message.should.equal('subti');
-          done();
-        })
-        return subti;
-      });
-      ti.onError(function (err) {
+      ctrl.run(function() {
         should(true).be.false;
       });
-      ti.run();
+    });
+
+    it('single async', function(done) {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(helper.singleAsyncFunc, function() {
+        throw new Error('aa');
+      });
+      ctrl.onError(function(err) {
+        err.message.should.equal('aa');
+        done();
+      });
+      ctrl.run(function() {
+        should(true).be.false;
+      });
+    });
+
+    it('multi args async', function(done) {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(helper.asyncMultiArgsFunc, 1, 2, 3, function() {
+        throw new Error('aa');
+      });
+      ctrl.onError(function(err) {
+        err.message.should.equal('aa');
+        done();
+      });
+      ctrl.run(function() {
+        should(true).be.false;
+      });
+    });
+
+    it('promise fail', function(done) {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(helper.createPromise('pr', true), function() {
+        throw new Error('aa');
+      });
+      ctrl.onError(function(err) {
+        err.message.should.equal('promise error');
+        done();
+      });
+      ctrl.run(function() {
+        should(true).be.false;
+      });
+    });
+
+    it('promise throw', function(done) {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(helper.createPromise('pr'), function() {
+        throw new Error('aa');
+      });
+      ctrl.onError(function(err) {
+        err.message.should.equal('aa');
+        done();
+      });
+      ctrl.run(function() {
+        should(true).be.false;
+      });
+    });
+
+  });
+
+
+
+  describe('nested error test', function() {
+    it('should use root error handler if sub tiny error handler is default', function(done) {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(function() {
+        var ctrl = new tiny.Controller();
+        ctrl.parallel(function() {
+          throw new Error('sub ctrl');
+        });
+        return ctrl;
+      });
+      ctrl.onError(function(err) {
+        err.message.should.equal('sub ctrl');
+        done();
+      });
+      ctrl.run();
+    });
+
+    it('should use sub error handler if sub tiny error handler is inited', function(done) {
+      var ctrl = new tiny.Controller();
+      ctrl.parallel(function() {
+        var ctrl = new tiny.Controller();
+        ctrl.parallel(function() {
+          throw new Error('sub ctrl');
+        });
+        ctrl.onError(function(err) {
+          err.message.should.equal('sub ctrl');
+          done();
+        })
+        return ctrl;
+      });
+      ctrl.onError(function(err) {
+        should(true).be.false;
+      });
+      ctrl.run();
     });
   });
 
